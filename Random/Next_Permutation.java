889400d6685df5ccd9997d33821da42a9db929fd
Nandan Upadhyaya <nandanupadhyaya18@gmail.com>
2025-08-12 22:28:40 +0530

    Next Permutation

---
+++ Random/Next_Permutation.java
@@ -0,0 +1,27 @@
+class Solution {
+    public void nextPermutation(int[] nums) {
+        int n = nums.length;
+        int i = n-2;
+        while(i >= 0 && nums[i] >= nums[i+1]) i--;
+        int j = n-1;
+        if(i >= 0){
+            while(j >= 0 && nums[i] >= nums[j]) j--;
+            swap(nums, i, j);
+        }
+        reverse(nums, i+1, n-1);
+    }
+
+    public static void swap(int[] nums, int i, int j){
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+
+    public static void reverse(int[] nums, int start, int end){
+        while(start < end){
+            swap(nums, start, end);
+            start++;
+            end--;
+        }
+    }
+}
