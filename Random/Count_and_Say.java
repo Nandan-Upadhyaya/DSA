02e82f665f7f542457c78b692f61591c31b632fd
Nandan Upadhyaya <nandanupadhyaya18@gmail.com>
2025-08-13 20:51:46 +0530

    Count and Say

---
+++ Random/Count_and_Say.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String countAndSay(int n) {
+       if(n == 1) return "1";
+       String prev = countAndSay(n-1);
+       StringBuilder sb = new StringBuilder();
+       int count = 1;
+       for(int i = 1 ; i<= prev.length() ; i++){
+        if(i < prev.length() && prev.charAt(i-1) == prev.charAt(i)) count++;
+        else{
+            sb.append(count).append(prev.charAt(i-1));
+            count = 1;
+        }
+       }
+       return sb.toString();
+    }
+}
