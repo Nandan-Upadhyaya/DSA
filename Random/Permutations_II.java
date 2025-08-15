266cd9128d143fd0cff01acd63969001a70bac1b
Nandan Upadhyaya <nandanupadhyaya18@gmail.com>
2025-08-15 11:10:43 +0530

    Permutations-II

---
+++ Random/Permutations_II.java
@@ -0,0 +1,28 @@
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+class Solution {
+    public List<List<Integer>> permuteUnique(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        List<Integer> current = new ArrayList<>();
+        for(int num : nums) current.add(num);
+        recursivebacktracking(nums, current, result, 0);
+        return result;
+    }
+    public static void recursivebacktracking(int[] nums, List<Integer> current, List<List<Integer>> result, int index){
+        
+        if(index == nums.length){
+            if(!result.contains(current)){
+            result.add(new ArrayList<>(current));
+            return;
+            };
+        }
+        for(int i = index ; i< nums.length ; i++){
+            Collections.swap(current, index, i);
+            recursivebacktracking(nums, current, result, index + 1);
+            Collections.swap(current, index, i);
+        }
+    }
+}
