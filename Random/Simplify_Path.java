87b88b6ffcaf9893e2b9ff44bc8874daae9b1a32
Nandan Upadhyaya <nandanupadhyaya18@gmail.com>
2025-08-17 11:19:08 +0530

    Simplify Path

---
+++ Random/Simplify_Path.java
@@ -0,0 +1,21 @@
+import java.util.Stack;
+class Solution {
+    public String simplifyPath(String path) {
+        String[] parts = path.split("/");
+        Stack<String> stack = new Stack<>();
+
+        for(String part : parts){
+            if(part.isEmpty() || part.equals(".")) continue;
+            else if(part.equals("..")) {
+                if(!stack.isEmpty()) stack.pop();
+            }
+            else stack.push(part);
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for(String part : stack){
+            sb.append("/").append(part);
+        }
+        return sb.toString().length() > 0 ? sb.toString() : "/";
+    }
+}
