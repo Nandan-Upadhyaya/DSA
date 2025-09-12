3181a16d0d9a87c25f809e30a40e19eedcf7eff5
Nandan Upadhyaya <nandanupadhyaya18@gmail.com>
2025-09-12 12:27:27 +0530

    Top K Frequent Elements

---
+++ Heap_and_Greedy_ and_Greedy_+_Heap/Top_K_Frequent_Elements.java
@@ -0,0 +1,21 @@
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.PriorityQueue;
+
+class Solution {
+    public int[] topKFrequent(int[] nums, int k) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int i = 0 ; i<nums.length ; i++) map.put(nums[i], map.getOrDefault(nums[i] , 0) + 1);
+
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b) -> a[1] - b[1]);
+
+        for(Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            pq.offer(new int[] {entry.getKey(), entry.getValue()});
+            if(pq.size() > k) pq.poll();
+        }
+        int[] res = new int[k];
+        for(int i = k - 1 ; i >= 0 ; i--) res[i] = pq.poll()[0];
+        return res;
+    }
+}
