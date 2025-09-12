5bc7ab488b14d284947d33bb46a6dfd5f8277f00
Nandan Upadhyaya <nandanupadhyaya18@gmail.com>
2025-09-12 17:05:41 +0530

    Meeting Rooms-I (corrected)

--- Random/Meeting_Room-I.java
+++ Random/Meeting_Room-I.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 class Solution {
 
     public static boolean canAttendMeetings(int[][] intervals) {
-        Arrays.sort(intervals, (a, b) -> a[0] - a[1]);
+        Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
         for(int i = 1 ; i<intervals.length ; i++) {
             if(intervals[i][0] < intervals[i-1][1]) return false;
         } 
